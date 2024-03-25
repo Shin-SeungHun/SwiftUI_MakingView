b62b08e4bc754456d63331b2effd0e6b52930aae
shin-SeungHun <sng7387@gmail.com>
2024-03-25 16:32:20 +0900

    feat: view 그리기

--- MakingView/ContentView.swift
+++ MakingView/ContentView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+     HelloText()
+        
     }
+    
 }
+
+// 해당하는 view 내에서만 사용할떄
+extension ContentView {
+        private var helloText: some View{
+            Text("안녕하세요")
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 20.0))
+                .padding([.vertical, .horizontal])
+        }
+    }
+
+// 어디서든 재사용 해야할때
+struct HelloText: View {
+    var body: some View {
+            Text("안녕하세요")
+                .foregroundStyle(.white)
+                .font(.largeTitle)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.blue)
+                .clipShape(RoundedRectangle(cornerRadius: 20.0))
+                .padding([.vertical, .horizontal])
+        }
+    }
 
 #Preview {
     ContentView()
