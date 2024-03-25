a2ed5048d4734e97966492d035f9a895d65f5b0f
shin-SeungHun <sng7387@gmail.com>
2024-03-25 16:29:41 +0900

    Initial Commit

---
+++ MakingView/MakingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakingViewApp.swift
+//  MakingView
+//
+//  Created by 신승훈 on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MakingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
